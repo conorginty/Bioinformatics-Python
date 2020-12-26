81ccb47a5821c6215231a1441db30bbf14bb83b9
Conor Ginty <conorginty@gmail.com>
2020-12-26 09:18:24 +0000

    Stronghold Problem 2: Transcribing DNA into RNA

---
+++ rosalind_problems/stronghold_problems/problem_2.py
@@ -0,0 +1,24 @@
+# Transcribing DNA into RNA
+
+# Problem
+# An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
+
+# Given a DNA string t corresponding to a coding strand, its transcribed RNA string u is formed by replacing all occurrences of 'T' in t with 'U' in u.
+
+# Given: A DNA string t having length at most 1000 nt. (nt = nucleotides)
+
+# Return: The transcribed RNA string of t.
+
+# To access the code from the dna_toolset folder
+import sys
+
+# Version 1
+# sys.path.insert(1, '/Users/conorginty/Desktop/Bioinformatics_in_Python/dna_toolset')
+
+# Version 2
+from os import path
+sys.path.append(path.dirname(path.dirname(path.dirname(path.realpath(__file__)))))
+
+from dna_toolset.dna_toolkit import transcription
+
+print(transcription("GATGGAACTTGACTACGTAAATT")) # GAUGGAACUUGACUACGUAAAUU
