29c5a019ec55582a3dde754bb98705181d8259ee
Conor Ginty <conorginty@gmail.com>
2021-01-04 16:14:51 +0000

    Added a timeit decorator to compare algorithm time costs

--- dna_toolset/decorate.py
+++ dna_toolset/decorate.py
@@ -1,2 +1,19 @@
+# Code created to make main.py output more legible
 def part_header(part, header_decoration="*"):
     print(f"{header_decoration * 10} {part} {header_decoration * 10}")
+
+# Code taken and modified from: https://medium.com/pythonhive/python-decorator-to-measure-the-execution-time-of-methods-fa04cb6bb36d
+import time
+
+def timeit(method):
+    def timed(*args, **kwargs):
+        ts = time.time()
+        result = method(*args, **kwargs)
+        te = time.time()
+        if 'log_time' in kwargs:
+            name = kwargs.get('log_name', method.__name__.upper())
+            kwargs['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
+        return result
+    return timed
